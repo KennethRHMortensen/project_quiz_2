12c6e2d07c939b09070066858f081f8bdf32a084
Kenneth <kenneth@kennt.dk>
2022-03-14 15:12:48 +0100

    added partials of head header and footer and created a simple navigation

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET index page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('pages/index', { title: 'Express' });
+});
+/* GET quiz setup page. */
+router.get('/quizsetup', function(req, res, next) {
+  res.render('pages/quizsetup', { 
+    title: 'Quiz setup page' });
+});
+/* GET test quiz page. */
+router.get('/quiztest', function(req, res, next) {
+  res.render('pages/quiztest', { 
+    title: 'Quiz testing page' });
+});
+/* GET quiz game page. */
+router.get('/quiz', function(req, res, next) {
+  res.render('pages/quiz', { 
+    title: 'Welcome to quiz game' });
 });
 
 module.exports = router;
+
+
